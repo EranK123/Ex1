8eb3f0007e40ba92a17de4495155bb473ffd79e4
EranK123 <93202645+EranK123@users.noreply.github.com>
2021-11-16 20:13:41 +0200

    Add files via upload

---
+++ src/CallsForElevator.py
@@ -0,0 +1,18 @@
+import csv
+
+from pip._internal.cli.cmdoptions import src
+
+
+class CallsForElevator:
+
+    def __init__(self, name, time, src, dest, type, allocated_to):
+        self.name = name
+        self.time = time
+        self.src = src
+        self.dest = dest
+        self.type = type
+        self.allocated_to = allocated_to
+
+    def __str__(self):
+        return f"time: {self.time} src: {self.src} dest: {self.dest} type: {self.type} allocated_to: {self.allocated_to}"
+
